17019de37b40d34cbe5c34708fb612da4a5f2fc8
Sekar243 <kinasekin6602@gmail.com>
2026-06-13 12:39:36 +0700

    ubah server

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.use((req, res, next) => {
 // Penanganan Error Global
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Terjadi kesalahan internal pada server.');
+  res.status(500).send(`Terjadi kesalahan internal pada server: ${err.message}<br><pre>${err.stack}</pre>`);
 });
 
 // Jalankan Server
